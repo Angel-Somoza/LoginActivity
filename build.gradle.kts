69d062491eab3f7a0a8fec684f19e6f91a516c29
Angel Somoza <somozacontrerasangeldavid@gmail.com>
2024-11-13 17:13:25 -0600

    se agregaron varias dependencias, dagger, navigation componet, mvvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false // Añade esto
+    id("androidx.navigation.safeargs") version "2.7.0" apply false  // Añade esto
+
 }
